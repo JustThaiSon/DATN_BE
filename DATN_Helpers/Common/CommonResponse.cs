a7f0bf9bcc86b6e2b7f3149c0943b376a8ab49a3
JustThaiSon <hoangthaisonqs@gmail.com>
2024-12-08 14:54:18 +0700

    setup Language en vi

---
+++ DATN_Helpers/Common/CommonResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN_Helpers.Common
+{
+    public class CommonResponse<T>
+    {
+        public int ResponseCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+    public class CommonPagination<T>
+    {
+        public int ResponseCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public int TotalRecord { get; set; }
+    }
+}
