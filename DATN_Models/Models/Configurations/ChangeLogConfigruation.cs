a060b05442434336ef43b5dba2dffd1f1ff51777
JustThaiSon <hoangthaisonqs@gmail.com>
2025-01-23 21:16:26 +0700

    add trigger

---
+++ DATN_Models/Models/Configurations/ChangeLogConfigruation.cs
@@ -0,0 +1,31 @@
+﻿using DATN_Models.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DATN_Models.Models.Configurations
+{
+    public class ChangeLogConfigruation : IEntityTypeConfiguration<ChangeLog>
+    {
+        public void Configure(EntityTypeBuilder<ChangeLog> builder)
+        {
+            builder.HasKey(e => e.Id); 
+
+            builder.Property(e => e.Action)
+                  .IsRequired()
+                  .HasMaxLength(100);
+
+            builder.Property(e => e.TableName)
+                  .IsRequired()
+                  .HasMaxLength(100);
+
+            builder.Property(e => e.BeforeChange)
+                  .HasColumnType("nvarchar(max)");
+
+            builder.Property(e => e.AfterChange)
+                  .HasColumnType("nvarchar(max)");
+
+            builder.Property(e => e.ChangeDateTime)
+                  .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
