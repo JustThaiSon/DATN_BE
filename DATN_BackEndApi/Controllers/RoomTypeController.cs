1020a1c7422e78274c65159a3ef53f4dc34af524
Thao <Thaonmph47496@fpt.edu.vn>
2025-03-26 00:20:19 +0700

    xoá phần statistic của nghĩa trong file interface sửa code phần room thêm code phần roomtype

---
+++ DATN_BackEndApi/Controllers/RoomTypeController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using DATN_Helpers.Common;
+using DATN_Helpers.Common.interfaces;
+using DATN_Helpers.Extensions;
+using DATN_Models.DAL.SeatType;
+using DATN_Models.DAO.Interface;
+using DATN_Models.DAO.Interface.SeatAbout;
+using DATN_Models.DTOS.RoomType.Res;
+using DATN_Models.DTOS.SeatType.Req;
+using DATN_Models.DTOS.SeatType.Res;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_BackEndApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomTypeController : ControllerBase
+    {
+        private readonly IRoomTypeDAO _roomTypeDAO;
+        private readonly string _langCode;
+        private readonly IUltil _ultils;
+        private readonly IMapper _mapper;
+
+        public RoomTypeController(IRoomTypeDAO roomTypeDAO, IConfiguration configuration, IUltil ultils, IMapper mapper)
+        {
+            _roomTypeDAO = roomTypeDAO;
+            _langCode = configuration["MyCustomSettings:LanguageCode"] ?? "vi";
+            _ultils = ultils;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("CreateRoomType")]
+
+        public async Task<CommonResponse<dynamic>> CreateRoomType(string rq)
+        {
+            var res = new CommonResponse<dynamic>();
+
+            _roomTypeDAO.CreateRoomType(rq, out int response);
+            res.Data = null;
+            res.Message = MessageUtils.GetMessage(response, _langCode);
+            res.ResponseCode = response;
+            return res;
+        }
+
+
+        [HttpPost]
+        [Route("DeleteRoomType")]
+
+        public async Task<CommonResponse<dynamic>> DeleteRoomType(Guid id)
+        {
+
+            var res = new CommonResponse<dynamic>();
+           _roomTypeDAO.DeleteRoomType(id, out int response);
+            res.Data = null;
+            res.Message = MessageUtils.GetMessage(response, _langCode);
+            res.ResponseCode = response;
+            return res;
+        }
+
+
+
+        [HttpGet]
+        [Route("GetListRoomType")]
+
+        public async Task<CommonPagination<List<RoomTypeGetListRes>>> GetListRoomType(int currentPage, int recordPerPage)
+        {
+
+            var res = new CommonPagination<List<RoomTypeGetListRes>>();
+
+            var result = _roomTypeDAO.GetListRoomType(currentPage, recordPerPage, out int TotalRecord, out int response);
+
+            var resultMapper = _mapper.Map<List<RoomTypeGetListRes>>(result);
+
+            res.Data = resultMapper;
+            res.Message = MessageUtils.GetMessage(response, _langCode);
+            res.ResponseCode = response;
+            res.TotalRecord = TotalRecord;
+
+            return res;
+        }
+
+
+    }
+}
---
+++ DATN_Models/Migrations/20250325160313_ad_roomtype.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DATN_Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class ad_roomtype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoomTypeId",
+                table: "Rooms",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "RoomTypes",
+                columns: table => new
+                {
+                    RoomTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomTypes", x => x.RoomTypeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomTypes");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "ShowTimes");
+
+            migrationBuilder.DropColumn(
+                name: "RoomTypeId",
+                table: "Rooms");
+        }
+    }
+}
